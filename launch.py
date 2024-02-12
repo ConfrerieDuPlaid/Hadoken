14d1d0c88841b05fb7a2e28f3d37260d929b847b
Sarah Schlegel <sschlegel@myges.fr>
2024-02-12 09:49:32 +0100

    rename files

--- launch.py
+++ launch.py
@@ -9,4 +9,4 @@ for noise in range(NOISES[0], NOISES[1] + 1, 5):
     for learning_rate in range(LEARNING_RATES[0], LEARNING_RATES[1] + 1, 5):
         for discount_factor in range(DISCOUNT_FACTORS[0], DISCOUNT_FACTORS[1] + 1, 5):
             # print(f"noise: {noise}, learning_rate: {learning_rate}, discount_factor: {discount_factor}")
-            Popen(f"python3 main.py {noise} {learning_rate} {discount_factor}", shell=True)
+            Popen(f"python3 no-graphic.py {noise} {learning_rate} {discount_factor}", shell=True)
